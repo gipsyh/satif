73a37ac3bfdee00d7315f58af41f68eb167b664e
Yuheng Su <gipsyh.icu@gmail.com>
2024-03-03 10:48:33 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use logic_form::{Lit, Var};
+use std::fmt::Debug;
 
-pub trait Solver {
+pub trait Satif {
+    type Model: SatifModel;
+    type Conflict: SatifConflict;
+
     fn new() -> Self;
 
     fn new_var(&mut self) -> Var;
@@ -9,22 +13,31 @@ pub trait Solver {
 
     fn add_clause(&mut self, clause: &[Lit]);
 
-    fn solve<M: Model, C: Conflict>(&mut self, assumps: &[Lit]) -> SatResult<M, C>;
+    fn solve(&mut self, assumps: &[Lit]) -> SatResult<Self::Model, Self::Conflict>;
 }
 
-pub trait Model {
+pub trait SatifModel {
     fn lit_value(&self, lit: Lit) -> Option<bool>;
 }
 
-pub trait Conflict {
+pub trait SatifConflict {
     fn has(&self, lit: Lit) -> bool;
 }
 
 pub enum SatResult<M, C>
 where
-    M: Model,
-    C: Conflict,
+    M: SatifModel,
+    C: SatifConflict,
 {
     Sat(M),
     Unsat(C),
+}
+
+impl<M: SatifModel, C: SatifConflict> Debug for SatResult<M, C> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Sat(_) => "Sat".fmt(f),
+            Self::Unsat(_) => "Unsat".fmt(f),
+        }
+    }
 }
